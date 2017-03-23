5cc4e2c8e3cef2ee229d6c24fb4443d34cd0d02a
Morgan Edmonds <morgan.edmonds@willowtreeapps.com>
2017-03-23 16:31:32 -0400

    Update clojure-sample.clj

--- clojure-sample.clj
+++ clojure-sample.clj
@@ -2,13 +2,13 @@
   (def turn-directions [:right :left :about-face])
   (def smoke-directions [:forward :backward :left :right :drop])
 
-  (let [command-options [(repeat 1 {:action :move
+  (let [command-options [(repeat 10 {:action :move
                                      :metadata {}})
-                         (repeat 2 {:action :turn
+                         (repeat 3 {:action :turn
                                     :metadata {:direction (rand-nth turn-directions)}})
-                         (repeat 4 {:action :shoot
+                         (repeat 15 {:action :shoot
                                       :metadata {}})
-                         (repeat 100 {:action :smoke
+                         (repeat 1 {:action :smoke
                                     :metadata {:direction (rand-nth smoke-directions)}})]]
 
     {:command (rand-nth (flatten command-options))
